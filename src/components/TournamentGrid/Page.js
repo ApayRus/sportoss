f1b2c21e865c818f649502b787c483ef766283f3
Aparus <rustamapaev@gmail.com>
2019-05-24 04:07:41 +0300

    added table for tournament categories grid

--- src/components/TournamentGrid/Page.js
+++ src/components/TournamentGrid/Page.js
@@ -4,16 +4,34 @@ import { connect } from "react-redux";
 import { firestoreConnect, isLoaded } from "react-redux-firebase";
 import { compose } from "redux";
 import { CircularProgress } from "@material-ui/core";
+import { categoryName } from "../../config/functions";
+import CategoriesTable from "../table/Table";
+
+const columns = [{ id: "name", numeric: false, disablePadding: false, label: "Категория" }];
 
 export class Page extends Component {
   render() {
-    const { tournament } = this.props;
-    console.log("this.props.tournament", this.props.tournament);
+    const { tournament, allCategories } = this.props;
+    //console.log("this.props.tournament", this.props.tournament);
+    let categories = [];
+    if (isLoaded(allCategories, tournament)) {
+      categories = allCategories.filter(cat => tournament.categories.includes(cat.id));
+      categories = categories.map(cat => ({ name: categoryName(cat) }));
+    }
     return (
       <div>
         {isLoaded(tournament) ? (
           <div>
             <h1>{`${tournament.name}, ${tournament.address}, ${tournament.date}`}</h1>
+            <CategoriesTable
+              data={categories}
+              columns={columns}
+              collection="categories"
+              title="Категории"
+              disableRowClick
+              hideToolbar
+              hideCheckboxes
+            />
           </div>
         ) : (
           <CircularProgress />
@@ -29,6 +47,7 @@ const mapStateToProps = state => {
 
   return {
     tournament: state.firestore.data.tournament,
+    allCategories: state.firestore.ordered.allCategories,
     user: { userId, userName }
   };
 };
@@ -37,6 +56,11 @@ export default compose(
   connect(mapStateToProps),
   firestoreConnect(props => {
     const { tournamentId } = props.match.params;
-    return [{ collection: "tournaments", doc: tournamentId, storeAs: "tournament" }];
+    return [
+      { collection: "tournaments", doc: tournamentId, storeAs: "tournament" },
+      { collection: "categories", storeAs: "allCategories" }
+    ];
   })
 )(Page);
+
+//return [{ collection: "athlets", where: [["createdBy.userId", "==", props.user.userId]] }];

e82260c6968d30171766c6d0718b0db88203bfed
Aparus <rustamapaev@gmail.com>
2019-05-25 03:23:11 +0300

    calculeted count of participants in each category

--- src/components/TournamentGrid/Page.js
+++ src/components/TournamentGrid/Page.js
@@ -5,18 +5,40 @@ import { firestoreConnect, isLoaded } from "react-redux-firebase";
 import { compose } from "redux";
 import { CircularProgress } from "@material-ui/core";
 import { categoryName } from "../../config/functions";
+import groupBy from "lodash/groupBy";
 import CategoriesTable from "../table/Table";
 
-const columns = [{ id: "name", numeric: false, disablePadding: false, label: "Категория" }];
+const columns = [
+  { id: "name", numeric: false, disablePadding: false, label: "Категория" },
+  { id: "participantsCount", numeric: false, disablePadding: false, label: "Участников" }
+];
+
+/**
+ * Returns count of participans of each category
+ */
+function countCategories(applications) {
+  let participants = {};
+  applications.forEach(app => {
+    participants = { ...participants, ...app.participants };
+  });
+  console.log("participants", participants);
+  return groupBy(participants, "categoryId");
+}
 
 export class Page extends Component {
   render() {
-    const { tournament, allCategories } = this.props;
-    //console.log("this.props.tournament", this.props.tournament);
+    const { tournament, allCategories, applications } = this.props;
     let categories = [];
-    if (isLoaded(allCategories, tournament)) {
+    if (isLoaded(allCategories, tournament, applications)) {
       categories = allCategories.filter(cat => tournament.categories.includes(cat.id));
-      categories = categories.map(cat => ({ name: categoryName(cat) }));
+      const countedCategories = countCategories(applications);
+      console.log("countedCategories", countedCategories);
+
+      categories = categories.map(cat => {
+        let participantsCount = 0;
+        if (countedCategories[cat.id]) participantsCount = countedCategories[cat.id].length;
+        return { id: cat.id, name: categoryName(cat), participantsCount };
+      });
     }
     return (
       <div>
@@ -48,6 +70,7 @@ const mapStateToProps = state => {
   return {
     tournament: state.firestore.data.tournament,
     allCategories: state.firestore.ordered.allCategories,
+    applications: state.firestore.ordered.applications,
     user: { userId, userName }
   };
 };
@@ -58,7 +81,8 @@ export default compose(
     const { tournamentId } = props.match.params;
     return [
       { collection: "tournaments", doc: tournamentId, storeAs: "tournament" },
-      { collection: "categories", storeAs: "allCategories" }
+      { collection: "categories", storeAs: "allCategories" },
+      { collection: "applications", where: [["tournamentId", "==", tournamentId]] }
     ];
   })
 )(Page);

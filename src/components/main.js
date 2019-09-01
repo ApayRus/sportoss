51854018ece5cb5257c6cba87ba509f8b18d330d
aparus <rustamapaev@gmail.com>
2019-09-01 16:43:51 +0300

    little gif added to main page

--- src/components/main.js
+++ src/components/main.js
@@ -2,7 +2,11 @@ import React, { Component } from "react";
 
 class App extends Component {
   render() {
-    return <div>Main</div>;
+    return (
+      <div style={{ textAlign: "center" }}>
+        <img alt="funny demo" width="100%" src="https://firebasestorage.googleapis.com/v0/b/zherebovka.appspot.com/o/brokedWall.webp?alt=media&token=29f32416-256c-4316-95fb-fc871ef1d298" />
+      </div>
+    );
   }
 }
 

bdb2e7f016d01a4cec5a25437fea28deeb8c26bd
aparus <rustamapaev@gmail.com>
2019-10-09 15:56:27 +0300

    Duel connected to redux store and  displays item(duel) from Grid finds participant by text "Name Family" and populates data

--- src/components/grid/Duel.js
+++ src/components/grid/Duel.js
@@ -1,6 +1,11 @@
 import React from 'react'
 import { withStyles } from '@material-ui/core/styles'
 import Checkbox from '@material-ui/core/Checkbox'
+import { connect } from 'react-redux'
+import { compose } from 'redux'
+import { find } from 'lodash'
+import { athletName, trainerName } from '../../config/functions'
+import { updateFighter } from '../../store/gridActions'
 
 const styles = {
   duel: {
@@ -23,7 +28,35 @@ const styles = {
 }
 
 function DuelSimple(props) {
-  const { id, classes } = props
+  const { duelData, classes, participants, updateFighter } = props
+  const { id, fighterRed, fighterBlue, winner } = duelData
+  let athletRedName,
+    athletBlueName,
+    trainerRedName,
+    trainerBlueName = ''
+
+  if (fighterRed) {
+    const participantRedPopulated = find(participants, { athlet: { id: fighterRed } })
+    athletRedName = athletName(participantRedPopulated.athlet)
+    trainerRedName = trainerName(participantRedPopulated.trainer)
+  }
+
+  if (fighterBlue) {
+    const participantBluePopulated = find(participants, { athlet: { id: fighterBlue } })
+    athletBlueName = athletName(participantBluePopulated.athlet)
+    trainerBlueName = trainerName(participantBluePopulated.trainer)
+  }
+
+  const onFighterChange = e => {
+    console.log(e.target.dataset.id, e.target.dataset.color, e.target.value)
+    const { id: duelId, color: fighterColor } = e.target.dataset
+    const [familyName, firstName] = e.target.value.split(' ')
+
+    const relatedParticipant = find(participants, { athlet: { familyName, firstName } })
+    const athletId = relatedParticipant ? relatedParticipant.athlet.id : ''
+    updateFighter({ duelId, fighterColor, athletId })
+  }
+
   return (
     <table className={classes.duel}>
       <tbody>
@@ -32,8 +65,16 @@ function DuelSimple(props) {
             {id}
           </td>
           <td className={classes.athletRed}>
-            <input type='text' placeholder='red' className={classes.athletInput} />
-            <div className={classes.trainer}>Trainer 1</div>
+            <input
+              onChange={onFighterChange}
+              type='text'
+              placeholder='red'
+              data-id={id}
+              data-color='Red'
+              className={classes.athletInput}
+              value={athletRedName}
+            />
+            <div className={classes.trainer}>{trainerRedName}</div>
           </td>
           <td className={classes.winnerRed}>
             <Checkbox style={{ width: 7, height: 7, color: 'red' }} />
@@ -41,8 +82,16 @@ function DuelSimple(props) {
         </tr>
         <tr>
           <td className={classes.athletBlue}>
-            <input type='text' placeholder='blue' className={classes.athletInput} />
-            <div className={classes.trainer}>Trainer 1</div>
+            <input
+              onChange={onFighterChange}
+              type='text'
+              placeholder='blue'
+              data-id={id}
+              data-color='Blue'
+              className={classes.athletInput}
+              value={athletBlueName}
+            />
+            <div className={classes.trainer}>{trainerBlueName}</div>
           </td>
           <td className={classes.winnerBlue}>
             <Checkbox style={{ width: 7, height: 7, color: 'blue' }} />
@@ -53,4 +102,18 @@ function DuelSimple(props) {
   )
 }
 
-export default withStyles(styles)(DuelSimple)
+const mapStateToProps = state => ({
+  participants: state.grid.participants
+})
+
+const mapDispatchToProps = dispatch => ({
+  updateFighter: payload => dispatch(updateFighter(payload))
+})
+
+export default compose(
+  withStyles(styles),
+  connect(
+    mapStateToProps,
+    mapDispatchToProps
+  )
+)(DuelSimple)

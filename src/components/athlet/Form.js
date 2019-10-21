595c6a1f46c3270afd7257cfb17597d3df76668d
aparus <rustamapaev@gmail.com>
2019-10-21 21:29:52 +0300

    refactored form: functionsal component with hooks

--- src/components/athlet/Form.js
+++ src/components/athlet/Form.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 import {
   Button,
@@ -13,152 +13,139 @@ import {
   DialogContent,
   DialogTitle
 } from '@material-ui/core'
-// import { Link, Redirect } from "react-router-dom";
-//import { connect } from "react-redux";
 
-class Form extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = { id: '', firstName: '', familyName: '', fatherName: '', birthday: '', gender: '' }
-  }
+function Form(props) {
+  const { isModalOpen, data, closeModal, firestoreAdd, firestoreUpdate, userId, userName } = props
 
-  componentDidMount() {
-    const { id, firstName, familyName, fatherName, birthday, gender } = this.props.data
-    if (id) {
-      this.setState({ id, firstName, familyName, fatherName, birthday, gender })
+  const [formState, setFormState] = useState({
+    id: '',
+    firstName: '',
+    familyName: '',
+    fatherName: '',
+    birthday: '',
+    gender: ''
+  })
+
+  useEffect(() => {
+    //component will mount
+    setFormState(data)
+    return () => {
+      //component will UNmount
     }
+  }, [])
+
+  const handleChange = field => e => {
+    setFormState({ ...formState, [field]: e.target.value.trim() })
   }
 
-  handleChange = e => {
-    this.setState({
-      [e.target.id]: e.target.value
-    })
-  }
-
-  handleSubmit = () => {
-    const { id, firstName, familyName, fatherName, birthday, gender } = this.state
-    const { userId, userName } = this.props
+  const handleSubmit = () => {
     const createdBy = { userId, userName }
     //id is empty when we creates new endtry, and filled when we edit an existen one
-    if (!id) {
-      const firestoreAdd = this.props.firestoreAdd(
-        { collection: 'athlets' },
-        { firstName, familyName, fatherName, birthday, gender, createdBy }
-      )
-      firestoreAdd.catch(error => {
+    if (!formState.id) {
+      firestoreAdd({ collection: 'athlets' }, { ...formState, createdBy }).catch(error => {
         console.log('firestoreAdd error', error)
       })
     } else {
-      const firestoreUpdate = this.props.firestoreUpdate(
-        { collection: 'athlets', doc: id },
-        { firstName, familyName, fatherName, birthday, gender, createdBy }
-      )
-      firestoreUpdate.catch(error => {
+      firestoreUpdate(
+        { collection: 'athlets', doc: formState.id },
+        { ...formState, createdBy }
+      ).catch(error => {
         console.log('firestoreUpdate error', error)
       })
     }
 
-    this.handleCancel()
+    handleCancel()
   }
 
-  handleCancel = () => {
-    this.props.closeModal()
+  const handleCancel = () => {
+    closeModal()
   }
 
-  render() {
-    const { id, firstName, familyName, fatherName, birthday, gender } = this.state
-    const formTitle = id ? 'Редактирование' : 'Добавление'
-    return (
-      <div>
-        <Dialog
-          open={this.props.isModalOpen}
-          onClose={this.handleClose}
-          aria-labelledby='form-dialog-title'
-        >
-          <DialogTitle id='form-dialog-title'>
-            <Typography color='primary'>{formTitle} спортсмена</Typography>
-          </DialogTitle>
-          <DialogContent>
-            <form onChange={this.handleChange}>
-              {/* FULLNAME */}
-              <TextField
-                id='familyName'
-                label='Фамилия'
-                type='text'
-                value={familyName}
-                margin='normal'
-                autoFocus
-                fullWidth
-                InputLabelProps={{
-                  shrink: true
-                }}
-              />
-              <TextField
-                id='firstName'
-                label='Имя'
-                type='text'
-                value={firstName}
-                margin='normal'
-                fullWidth
-                InputLabelProps={{
-                  shrink: true
-                }}
-              />
-              <TextField
-                id='fatherName'
-                label='Отчество'
-                type='text'
-                value={fatherName}
-                margin='normal'
-                fullWidth
-                InputLabelProps={{
-                  shrink: true
-                }}
-              />
-              <br />
-              {/* BIRTHDAY */}
-              <TextField
-                id='birthday'
-                label='Дата рождения'
-                type='date'
-                value={birthday}
-                margin='normal'
-                fullWidth
-                InputLabelProps={{
-                  shrink: true
-                }}
-              />
-              {/* GENDER */}
-              <FormControl fullWidth>
-                <InputLabel htmlFor='gender'>Пол</InputLabel>
-                <Select
-                  native //if remove that, id does"t appear in event.target
-                  value={gender}
-                  inputProps={{
-                    id: 'gender'
-                  }}
-                >
-                  <option value='' />
-                  <option value='Муж'>Муж</option>
-                  <option value='Жен'>Жен</option>
-                </Select>
-              </FormControl>
-            </form>
+  const formTitle = formState.id ? 'Редактирование' : 'Добавление'
+  return (
+    <div>
+      <Dialog open={isModalOpen} onClose={closeModal} aria-labelledby='form-dialog-title'>
+        <DialogTitle id='form-dialog-title'>
+          <Typography color='primary'>{formTitle} спортсмена</Typography>
+        </DialogTitle>
+        <DialogContent>
+          <form>
+            {/* FULLNAME */}
+            <TextField
+              onChange={handleChange('familyName')}
+              label='Фамилия'
+              type='text'
+              value={formState.familyName}
+              margin='normal'
+              autoFocus
+              fullWidth
+              InputLabelProps={{
+                shrink: true
+              }}
+            />
+            <TextField
+              onChange={handleChange('firstName')}
+              label='Имя'
+              type='text'
+              value={formState.firstName}
+              margin='normal'
+              fullWidth
+              InputLabelProps={{
+                shrink: true
+              }}
+            />
+            <TextField
+              onChange={handleChange('fatherName')}
+              label='Отчество'
+              type='text'
+              value={formState.fatherName}
+              margin='normal'
+              fullWidth
+              InputLabelProps={{
+                shrink: true
+              }}
+            />
             <br />
-            <FormHelperText> {/*THIS IS PLACE FOR ERROR MESSAGE */}</FormHelperText>
-          </DialogContent>
-          <DialogActions>
-            <Button onClick={this.handleCancel} color='default'>
-              Отмена
-            </Button>
-            <Button onClick={this.handleSubmit} color='primary'>
-              Сохранить
-            </Button>
-          </DialogActions>
-        </Dialog>
-      </div>
-    )
-  }
+            {/* BIRTHDAY */}
+            <TextField
+              onChange={handleChange('birthday')}
+              label='Дата рождения'
+              type='date'
+              value={formState.birthday}
+              margin='normal'
+              fullWidth
+              InputLabelProps={{
+                shrink: true
+              }}
+            />
+            {/* GENDER */}
+            <FormControl fullWidth>
+              <InputLabel htmlFor='gender'>Пол</InputLabel>
+              <Select
+                native //if remove that, id does"t appear in event.target
+                value={formState.gender}
+                onChange={handleChange('gender')}
+              >
+                <option value='' />
+                <option value='Муж'>Муж</option>
+                <option value='Жен'>Жен</option>
+              </Select>
+            </FormControl>
+          </form>
+          <br />
+          <FormHelperText> {/*THIS IS PLACE FOR ERROR MESSAGE */}</FormHelperText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleCancel} color='default'>
+            Отмена
+          </Button>
+          <Button onClick={handleSubmit} color='primary'>
+            Сохранить
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  )
 }
 
 export default Form

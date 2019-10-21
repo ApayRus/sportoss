644a5958fa7f82868ab17cd7126882fbfcff7edc
aparus <rustamapaev@gmail.com>
2019-10-21 22:14:26 +0300

    refactored to functional component, using hooks

--- src/components/category/Page.js
+++ src/components/category/Page.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import { Fab, CircularProgress } from '@material-ui/core'
 import AddIcon from '@material-ui/icons/Add'
 
@@ -13,64 +13,59 @@ import { categoryName } from '../../config/functions'
 //Table columns or fields of our data model
 const columns = [{ id: 'categoryName', numeric: false, disablePadding: false, label: 'Категория' }]
 
-export class Page extends Component {
-  state = { isModalOpen: false, data: {} }
+function Page(props) {
+  const { categories } = props
 
-  openModal = id => {
-    const defaultData = { gender: '', minAge: '', maxAge: '', weight: '' } // if we create new entry
-    const modalData = this.props.categories.find(el => el.id === id) || defaultData
-    this.setState({ modalData })
-    this.setState({ isModalOpen: true })
+  const [isModalOpen, setModalOpen] = useState(false)
+  const [modalData, setModalData] = useState({ gender: '', minAge: '', maxAge: '', weight: '' })
+
+  const openModal = id => {
+    const modalData = categories.find(el => el.id === id)
+    setModalData(modalData)
+    setModalOpen(true)
   }
 
-  closeModal = () => {
-    this.setState({ isModalOpen: false })
+  const closeModal = () => {
+    setModalOpen(false)
+  }
+  let tableData = []
+  if (isLoaded(categories)) {
+    tableData = categories.map(cat => {
+      return { id: cat.id, categoryName: categoryName(cat) }
+    })
   }
 
-  render() {
-    let { categories } = this.props
+  const { add: firestoreAdd, delete: firestoreDelete, update: firestoreUpdate } = props.firestore
 
-    if (isLoaded(categories)) {
-      categories = categories.map(cat => {
-        return { id: cat.id, categoryName: categoryName(cat) }
-      })
-    }
-    const {
-      add: firestoreAdd,
-      delete: firestoreDelete,
-      update: firestoreUpdate
-    } = this.props.firestore
-
-    return (
-      <main>
-        {isLoaded(categories) ? (
-          <Table
-            data={categories}
-            // handleSelected={this.getSelected}
-            openModal={this.openModal}
-            firestoreDelete={firestoreDelete}
-            columns={columns}
-            collection='categories'
-            title='Категории'
-          />
-        ) : (
-          <CircularProgress />
-        )}
-        <Fab style={fabStyle} onClick={() => this.openModal(null)} color='primary' aria-label='Add'>
-          <AddIcon />
-        </Fab>
-        {this.state.isModalOpen && (
-          <Form
-            isModalOpen={this.state.isModalOpen}
-            data={this.state.modalData}
-            closeModal={this.closeModal}
-            firestoreAdd={firestoreAdd}
-            firestoreUpdate={firestoreUpdate}
-          />
-        )}
-      </main>
-    )
-  }
+  return (
+    <main>
+      {isLoaded(categories) ? (
+        <Table
+          data={tableData}
+          // handleSelected={this.getSelected}
+          openModal={openModal}
+          firestoreDelete={firestoreDelete}
+          columns={columns}
+          collection='categories'
+          title='Категории'
+        />
+      ) : (
+        <CircularProgress />
+      )}
+      <Fab style={fabStyle} onClick={() => openModal(null)} color='primary' aria-label='Add'>
+        <AddIcon />
+      </Fab>
+      {isModalOpen && (
+        <Form
+          isModalOpen={isModalOpen}
+          data={modalData}
+          closeModal={closeModal}
+          firestoreAdd={firestoreAdd}
+          firestoreUpdate={firestoreUpdate}
+        />
+      )}
+    </main>
+  )
 }
 
 const mapStateToProps = state => {

2e08c2a57274b96ba0da70ab223f2be12fd669ac
aparus <rustamapaev@gmail.com>
2019-10-22 02:37:45 +0300

    form refactored to functional component with hooks

--- src/components/category/Form.js
+++ src/components/category/Form.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from 'react'
 
 import {
   Button,
@@ -11,9 +11,8 @@ import {
   DialogContent,
   DialogTitle,
   FormControl
-} from "@material-ui/core";
+} from '@material-ui/core'
 // import { Link, Redirect } from "react-router-dom";
-import { connect } from "react-redux";
 
 const styles = {
   categoryInput: { width: 40, margin: 5 },
@@ -21,153 +20,114 @@ const styles = {
     marginRight: 10,
     marginLeft: 10,
     marginTop: 6,
-    display: "inline-block"
+    display: 'inline-block'
   },
   categoryContainer: {
     /* display: "flex", alignItems: "center" */
   }
-};
-
-class Form extends React.Component {
-  state = {
-    id: "",
-    gender: "",
-    minAge: "",
-    maxAge: "",
-    minWeight: "",
-    maxWeight: ""
-  };
-
-  componentDidMount() {
-    const { id, gender, minAge, maxAge, weight } = this.props.data;
-    this.setState({ id, gender, minAge, maxAge, weight });
-  }
-
-  handleChange = e => {
-    //console.log("event.target", e.target);
-    this.setState({
-      [e.target.id]: e.target.value
-    });
-  };
-
-  handleSubmit = () => {
-    // category = { id, gender, minAge, maxAge, weight }
-    const { id, gender, minAge, maxAge, weight } = this.state;
-    const createdBy = {
-      userName: this.props.profile.username,
-      userId: this.props.auth.uid
-    };
-    //id is empty when we creates new endtry, and filled when we edit an existen one
-    if (!id) {
-      const firestoreAdd = this.props.firestoreAdd(
-        { collection: "categories" },
-        { gender, minAge, maxAge, weight, createdBy }
-      );
-      firestoreAdd.catch(error => {
-        console.log("firestoreAdd error", error.message);
-      });
-    } else {
-      const firestoreUpdate = this.props.firestoreUpdate(
-        { collection: "categories", doc: id },
-        { id, gender, minAge, maxAge, weight, createdBy }
-      );
-      firestoreUpdate.catch(error => {
-        console.log("firestoreUpdate error", error.message);
-      });
-    }
-
-    this.handleCancel();
-  };
-
-  handleCancel = () => {
-    this.props.closeModal();
-  };
-
-  render() {
-    const { id, gender, minAge, maxAge, weight } = this.state;
-    //console.log("gender", gender);
-    const formTitle = id ? "Редактирование" : "Добавление";
-    return (
-      <Dialog
-        open={this.props.isModalOpen}
-        onClose={this.handleClose}
-        aria-labelledby="form-dialog-title"
-      >
-        <DialogTitle id="form-dialog-title">
-          <Typography color="primary">{formTitle} категории</Typography>
-        </DialogTitle>
-        <DialogContent>
-          <form style={styles.categoryContainer} onChange={this.handleChange}>
-            <Typography style={styles.categoryTypography}>Пол</Typography>
-            <FormControl>
-              <Select
-                native
-                value={gender}
-                inputProps={{
-                  id: "gender"
-                }}
-              >
-                <option value="" />
-                <option value="Муж">Муж</option>
-                <option value="Жен">Жен</option>
-              </Select>
-            </FormControl>
-            <br />
-            <Typography style={{ ...styles.categoryTypography, marginTop: 11 }}>
-              Возраст
-            </Typography>
-            <TextField
-              id="minAge"
-              value={minAge}
-              placeholder="от"
-              type="number"
-              style={styles.categoryInput}
-            />
-            {` - `}
-            <TextField
-              placeholder="до"
-              type="number"
-              id="maxAge"
-              value={maxAge}
-              style={styles.categoryInput}
-            />
-            <br />
-            <Typography style={{ ...styles.categoryTypography, marginTop: 11 }}>
-              Вес
-            </Typography>
-            <TextField
-              placeholder="вес"
-              type="text"
-              id="weight"
-              value={weight}
-              margin="normal"
-              style={styles.categoryInput}
-            />
-          </form>
-          <br />
-          <FormHelperText>
-            {" "}
-            {/*THIS IS PLACE FOR ERROR MESSAGE */}
-          </FormHelperText>
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={this.handleCancel} color="primary">
-            Cancel
-          </Button>
-          <Button onClick={this.handleSubmit} color="primary">
-            Save
-          </Button>
-        </DialogActions>
-      </Dialog>
-    );
-  }
 }
 
-const mapStateToProps = state => {
-  return {
-    auth: state.firebase.auth,
-    profile: state.firebase.profile
-  };
-};
+function Form(props) {
+  const { isModalOpen, data, closeModal, firestoreAdd, firestoreUpdate, userId, userName } = props
 
-export default connect(mapStateToProps)(Form);
+  const [formState, setFormState] = useState({})
+
+  useEffect(() => {
+    //component will mount
+    setFormState(data)
+    return () => {
+      //component will UNmount
+    }
+  }, [])
+
+  const handleChange = field => e => {
+    setFormState({ ...formState, [field]: e.target.value.trim() })
+  }
+
+  const handleSubmit = () => {
+    // category = { id, gender, minAge, maxAge, weight }
+    const createdBy = {
+      userName,
+      userId
+    }
+    //id is empty when we creates new endtry, and filled when we edit an existen one
+    if (!formState.id) {
+      firestoreAdd({ collection: 'categories' }, { ...formState, createdBy }).catch(error => {
+        console.log('firestoreAdd error', error.message)
+      })
+    } else {
+      firestoreUpdate(
+        { collection: 'categories', doc: formState.id },
+        { ...formState, createdBy }
+      ).catch(error => {
+        console.log('firestoreUpdate error', error.message)
+      })
+    }
+
+    handleCancel()
+  }
+
+  const handleCancel = () => {
+    closeModal()
+  }
+
+  const formTitle = formState.id ? 'Редактирование' : 'Добавление'
+  return (
+    <Dialog open={isModalOpen} onClose={closeModal} aria-labelledby='form-dialog-title'>
+      <DialogTitle id='form-dialog-title'>
+        <Typography color='primary'>{formTitle} категории</Typography>
+      </DialogTitle>
+      <DialogContent>
+        <form style={styles.categoryContainer}>
+          <Typography style={styles.categoryTypography}>Пол</Typography>
+          <FormControl>
+            <Select native value={formState.gender} onChange={handleChange('gender')}>
+              <option value='' />
+              <option value='Муж'>Муж</option>
+              <option value='Жен'>Жен</option>
+            </Select>
+          </FormControl>
+          <br />
+          <Typography style={{ ...styles.categoryTypography, marginTop: 11 }}>Возраст</Typography>
+          <TextField
+            onChange={handleChange('minAge')}
+            value={formState.minAge}
+            placeholder='от'
+            type='number'
+            style={styles.categoryInput}
+          />
+          {` - `}
+          <TextField
+            placeholder='до'
+            type='number'
+            onChange={handleChange('maxAge')}
+            value={formState.maxAge}
+            style={styles.categoryInput}
+          />
+          <br />
+          <Typography style={{ ...styles.categoryTypography, marginTop: 11 }}>Вес</Typography>
+          <TextField
+            placeholder='вес'
+            type='text'
+            onChange={handleChange('weight')}
+            value={formState.weight}
+            margin='normal'
+            style={styles.categoryInput}
+          />
+        </form>
+        <br />
+        <FormHelperText> {/*THIS IS PLACE FOR ERROR MESSAGE */}</FormHelperText>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={handleCancel} color='primary'>
+          Cancel
+        </Button>
+        <Button onClick={handleSubmit} color='primary'>
+          Save
+        </Button>
+      </DialogActions>
+    </Dialog>
+  )
+}
+
+export default Form

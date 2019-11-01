5d595c93055edb3fac55515c9d87aa2822820005
aparus <rustamapaev@gmail.com>
2019-11-01 18:12:04 +0300

    changed styles

--- src/components/grid/Duel.jsx
+++ src/components/grid/Duel.jsx
@@ -1,58 +1,63 @@
 import React from 'react'
-import { withStyles } from '@material-ui/core/styles'
 import { Checkbox, Box } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles'
 import { find } from 'lodash'
 import { athletName, trainerName } from '../../config/functions'
-
-const styles = {
-  duelTable: {
-    /* width: '5cm', */
-    border: '1px solid gray',
-    borderRadius: 5,
-    marginBottom: 5,
-    borderSpacing: 0,
-    tableLayout: 'fixed'
-  },
-  duelNumber: {
-    width: 30,
-    borderRight: '1px solid gray',
-    textAlign: 'center',
-    color: 'slategray'
-  },
-  duelLabel: {
-    fontSize: 9
-  },
-  athletRed: { width: 140, height: 33, padding: '0 5px', borderBottom: '1px solid gray' },
-  athletBlue: { width: 140, height: 33, padding: '0 5px' },
-  athletInput: { width: '100%', border: 'none' },
-  checkboxColumn: { width: 35, height: 33 },
-  checkboxColumnRed: { borderBottom: '1px solid gray' },
-  checkboxRed: { width: 7, height: 7, marginTop: 2, color: 'red' },
-  checkboxBlue: { width: 7, height: 7, marginTop: 2, color: 'blue' },
-  trainer: { color: 'gray', fontSize: 10 },
-  athletColorColumn: { width: 5 },
-  athletColorRed: { backgroundColor: 'rgba(255,0,0,0.5)', borderBottom: ' 1px solid gray' },
-  athletColorBlue: { backgroundColor: 'rgba(0,0,255,0.5)' },
-  scoreColumn: { width: 35, borderLeft: ' 1px solid gray' },
-  scoreColumnRed: {
-    '& input': { color: 'rgba(255,0,0,0.5)', fontWeight: 'bold' },
-    borderBottom: ' 1px solid gray'
-  },
-  scoreColumnBlue: { '& input': { color: 'rgba(0,0,255,0.5)', fontWeight: 'bold' } },
-  scoreInput: { border: 'none', width: 31, height: 25, textAlign: 'center' }
-}
 
 function DuelSimple(props) {
   const {
     duelData,
-    classes,
     participants,
     onFighterChange,
     onWinnerChange,
     showScoreInput,
     showWinnerCheckbox
   } = props
-  const { id, fighterRed, fighterBlue, winner, label, scoreRed, scoreBlue } = duelData
+  const { id, fighterRed, fighterBlue, winner, label /* scoreRed, scoreBlue  */ } = duelData
+
+  const styles = {
+    duelTable: props => ({
+      /* width: '5cm', */
+      border: '1px solid gray',
+      borderRadius: 5,
+      marginBottom: 5,
+      borderSpacing: 0,
+      tableLayout: 'fixed',
+      visibility: props.duelData.status === 'fake' ? 'hidden' : 'visible'
+    }),
+    duelNumber: {
+      width: 30,
+      borderRight: '1px solid gray',
+      textAlign: 'center',
+      color: 'slategray'
+    },
+    duelLabel: {
+      fontSize: 9
+    },
+    athletRed: { width: 140, height: 33, padding: '0 5px', borderBottom: '1px solid gray' },
+    athletBlue: { width: 140, height: 33, padding: '0 5px' },
+    athletInput: { width: '100%', border: 'none' },
+    checkboxColumn: { width: 35, height: 33 },
+    checkboxColumnRed: { borderBottom: '1px solid gray' },
+    checkboxRed: { width: 7, height: 7, marginTop: 2, color: 'red' },
+    checkboxBlue: { width: 7, height: 7, marginTop: 2, color: 'blue' },
+    trainer: { color: 'gray', fontSize: 10 },
+    athletColorColumn: { width: 5 },
+    athletColorRed: { backgroundColor: 'rgba(255,0,0,0.5)', borderBottom: ' 1px solid gray' },
+    athletColorBlue: { backgroundColor: 'rgba(0,0,255,0.5)' },
+    scoreColumn: { width: 35, borderLeft: ' 1px solid gray' },
+    scoreColumnRed: {
+      '& input': { color: 'rgba(255,0,0,0.5)', fontWeight: 'bold' },
+      borderBottom: ' 1px solid gray'
+    },
+    scoreColumnBlue: { '& input': { color: 'rgba(0,0,255,0.5)', fontWeight: 'bold' } },
+    scoreInput: { border: 'none', width: 31, height: 25, textAlign: 'center' }
+  }
+
+  const useStyles = makeStyles(theme => styles)
+
+  const classes = useStyles(props)
+
   let athletRedName, athletBlueName, trainerRedName, trainerBlueName
 
   //populate fighters data by id
@@ -145,4 +150,4 @@ function DuelSimple(props) {
   )
 }
 
-export default withStyles(styles)(DuelSimple)
+export default DuelSimple

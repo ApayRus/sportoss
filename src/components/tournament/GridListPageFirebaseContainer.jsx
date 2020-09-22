3ecd3f5bb3660fa62876a083200f7e5a9557940f
aparus <rustamapaev@gmail.com>
2020-09-22 12:15:01 +0300

    rewrite with hooks

--- src/components/tournament/GridListPageFirebaseContainer.jsx
+++ src/components/tournament/GridListPageFirebaseContainer.jsx
@@ -1,44 +1,23 @@
 import React from 'react'
 import { map } from 'lodash'
 
-import { connect } from 'react-redux'
-import { firestoreConnect, isLoaded } from 'react-redux-firebase'
-import { compose } from 'redux'
+import { useFirestoreConnect, isLoaded } from 'react-redux-firebase'
 import ListPage from './GridListPage'
 import { CircularProgress } from '@material-ui/core'
+import { useSelector } from 'react-redux'
 
 export function Page(props) {
-	const { tournament, allCategories, applications, grids, profile } = props
-	const loadedProps = { tournament, allCategories, applications, grids: grids || {}, profile }
-	if (isLoaded(allCategories, tournament, applications, grids)) {
-		return <ListPage {...loadedProps} />
-	} else {
-		return <CircularProgress />
-	}
-}
+	const { tournament, categories, grids } = useSelector(state => state.firestore.data)
+	const { applications } = useSelector(state => state.firestore.ordered)
+	const { profile } = useSelector(state => state.firebase)
+	const allCategories = map(categories, (elem, key) => ({ id: key, ...elem }))
 
-const mapStateToProps = state => {
-	const categories = map(state.firestore.data.categories, (elem, key) => ({
-		id: key,
-		...elem
-	}))
-	return {
-		tournament: state.firestore.data.tournament,
-		allCategories: categories,
-		applications: state.firestore.ordered.applications,
-		grids: state.firestore.data.grids,
-		profile: state.firebase.profile
-	}
-}
-
-export default compose(
-	connect(mapStateToProps),
-	firestoreConnect(props => {
+	useFirestoreConnect(() => {
 		const { tournamentId } = props.match.params
 		return [
 			{ collection: 'tournaments', doc: tournamentId, storeAs: 'tournament' },
 			{ collection: 'grids', doc: `${tournamentId}`, storeAs: 'grids' },
-			{ collection: 'categories', doc: props.profile.club, storeAs: 'categories' },
+			{ collection: 'categories', doc: profile.club, storeAs: 'categories' },
 			{
 				collection: 'applications',
 				where: [['tournamentId', '==', tournamentId]],
@@ -46,4 +25,14 @@ export default compose(
 			}
 		]
 	})
-)(Page)
+
+	const loadedProps = { tournament, allCategories, applications, grids: grids || {}, profile }
+
+	if (isLoaded(allCategories, tournament, applications, grids)) {
+		return <ListPage {...loadedProps} />
+	} else {
+		return <CircularProgress />
+	}
+}
+
+export default Page

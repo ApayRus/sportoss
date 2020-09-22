dd99d58d15a162c909bf082ce28a067b27f0ce5f
aparus <rustamapaev@gmail.com>
2020-09-22 05:11:24 +0300

    fix when saved grid for tournament doesn't exist

--- src/components/tournament/GridListPageFirebaseContainer.jsx
+++ src/components/tournament/GridListPageFirebaseContainer.jsx
@@ -8,42 +8,42 @@ import ListPage from './GridListPage'
 import { CircularProgress } from '@material-ui/core'
 
 export function Page(props) {
-  const { tournament, allCategories, applications, grids, profile } = props
-  const loadedProps = { tournament, allCategories, applications, grids, profile }
-  if (isLoaded(allCategories, tournament, applications, grids)) {
-    return <ListPage {...loadedProps} />
-  } else {
-    return <CircularProgress />
-  }
+	const { tournament, allCategories, applications, grids, profile } = props
+	const loadedProps = { tournament, allCategories, applications, grids: grids || {}, profile }
+	if (isLoaded(allCategories, tournament, applications, grids)) {
+		return <ListPage {...loadedProps} />
+	} else {
+		return <CircularProgress />
+	}
 }
 
 const mapStateToProps = state => {
-  const categories = map(state.firestore.data.categories, (elem, key) => ({
-    id: key,
-    ...elem
-  }))
-  return {
-    tournament: state.firestore.data.tournament,
-    allCategories: categories,
-    applications: state.firestore.ordered.applications,
-    grids: state.firestore.data.grids,
-    profile: state.firebase.profile
-  }
+	const categories = map(state.firestore.data.categories, (elem, key) => ({
+		id: key,
+		...elem
+	}))
+	return {
+		tournament: state.firestore.data.tournament,
+		allCategories: categories,
+		applications: state.firestore.ordered.applications,
+		grids: state.firestore.data.grids,
+		profile: state.firebase.profile
+	}
 }
 
 export default compose(
-  connect(mapStateToProps),
-  firestoreConnect(props => {
-    const { tournamentId } = props.match.params
-    return [
-      { collection: 'tournaments', doc: tournamentId, storeAs: 'tournament' },
-      { collection: 'grids', doc: `${tournamentId}`, storeAs: 'grids' },
-      { collection: 'categories', doc: props.profile.club, storeAs: 'categories' },
-      {
-        collection: 'applications',
-        where: [['tournamentId', '==', tournamentId]],
-        storeAs: 'applications'
-      }
-    ]
-  })
+	connect(mapStateToProps),
+	firestoreConnect(props => {
+		const { tournamentId } = props.match.params
+		return [
+			{ collection: 'tournaments', doc: tournamentId, storeAs: 'tournament' },
+			{ collection: 'grids', doc: `${tournamentId}`, storeAs: 'grids' },
+			{ collection: 'categories', doc: props.profile.club, storeAs: 'categories' },
+			{
+				collection: 'applications',
+				where: [['tournamentId', '==', tournamentId]],
+				storeAs: 'applications'
+			}
+		]
+	})
 )(Page)

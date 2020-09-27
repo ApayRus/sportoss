813a2653067e5d5f8eb999ddf08d54b76e342737
aparus <rustamapaev@gmail.com>
2020-09-27 14:07:25 +0300

    little change and rename

---
+++ src/components/main.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import { Typography } from '@material-ui/core'
+class App extends Component {
+	render() {
+		return (
+			<div style={{ textAlign: 'center' }}>
+				<Typography variant='body1'>Главная страница</Typography>
+			</div>
+		)
+	}
+}
+
+export default App
